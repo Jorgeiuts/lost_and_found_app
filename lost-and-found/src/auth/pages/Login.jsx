c09cde113e9d690159cf0626775d2453131e7266
Jorge Alonso Castañeda Sotelo <jorge.alonso241099@gmail.com>
2025-03-03 23:57:32 -0600

    Se agrega todo el diseno de la ventana de login

--- lost-and-found/src/auth/pages/Login.jsx
+++ lost-and-found/src/auth/pages/Login.jsx
@@ -1,5 +1,28 @@
+import '/assets/css/login.css'
+
 export const Login = () => {
   return (
-    <h1>Login</h1>
+    <div className="container-fluid row">
+      <div className="col-md-5 left-panel">
+        <img src="/assets/logos/logo_liceo_bco.png" alt="Logo Liceo del Valle"/>
+      </div>
+      <div className="col-md-7 right-panel">
+        <h3 className="text-center fw-bold text-login">INICIAR SESIÓN</h3>
+        <p className="text-center text-muted text-welcome">Bienvenido a Lost and Found</p>
+        <form>
+                <div className="mb-3 d-flex align-items-center">
+                    <img className="icons" src="/assets/icons/icon_user.png" alt=""/>
+                    <input type="email" className="hbox" placeholder="Correo electrónico"/>
+                </div>
+                <div className="mb-3 d-flex align-items-center">
+                    <img className="icons" src="/assets/icons/icon_password.png" alt=""/>
+                    <input type="password" className="hbox" placeholder="Contraseña"/>
+                </div>
+                <div className="container d-flex justify-content-end">
+                    <button type="submit" className="btn btn-primary">Iniciar sesión</button>
+                </div>
+        </form>
+      </div>
+    </div>
   )
 }
--- lost-and-found/src/main.jsx
+++ lost-and-found/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { AppRouter } from './router/AppRouter';
-import './styles.css'
 
 
 
