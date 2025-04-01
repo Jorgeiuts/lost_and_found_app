b94fed65da2f2707b6836c3fd6e8dfbe1e6a2a99
Jorge Alonso Castañeda Sotelo <jorge.alonso241099@gmail.com>
2025-04-01 17:11:32 -0600

    Se agrega axios y redux al proyecto

--- lost-and-found/src/main.jsx
+++ lost-and-found/src/main.jsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { AppRouter } from './router/AppRouter';
+import { store } from './store';
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
