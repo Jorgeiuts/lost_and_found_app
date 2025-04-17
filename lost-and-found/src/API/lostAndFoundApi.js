462ee1bf3c80db48fc8da124bbc2b335151c28cc
Jorge Alonso Castañeda Sotelo <jorge.alonso241099@gmail.com>
2025-04-17 13:24:39 -0600

    Se configura la conexion mediante axios para enviar el token

--- lost-and-found/src/API/lostAndFoundApi.js
+++ lost-and-found/src/API/lostAndFoundApi.js
@@ -7,4 +7,12 @@ const lostAndFoundApi = axios.create({
     baseURL: VITE_API_URL
 })
 
+lostAndFoundApi.interceptors.request.use( config => {
+    const token = localStorage.getItem('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 export default lostAndFoundApi;
